0cac9b3e060924a627978651a1755bf31f16fb4b
Thalyson1 <Trungr@hotmail.com>
2022-03-23 20:24:10 -0300

    Criei um formulário, o salvei em um local storage. Criei uma segunda págnia 'teste.html' e criei um botao para puxar as informações do formulario da pagina 'index.hmtl' e mostrar para o usuario

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
-fetch('http://localhost:3000').then(resposta => resposta.json()).then((json) =>{
+fetch('http://localhost:3000').then(resposta => resposta.json()).then((json) => {
     console.log(json);
 });
+
+let musica_input = document.querySelector("#musica");
+let autor_input = document.querySelector("#autor");
+let ano_input = document.querySelector("#ano");
+
+
+let lista = document.querySelector("#flista");
+
+let musicas = [];
+
+let msc_output = [];
+
+let tam_msc = [];
+
+function mostrardados() {
+
+    for (musica of musicas) {
+        let eltarefa = document.createElement('li');
+        let txttarefa = document.createTextNode(musica);
+
+        eltarefa.appendChild(txttarefa);
+        lista.appendChild(eltarefa);
+    }
+
+}
+
+function adiciona() {
+    msc_output.push(`Musica: ` + musica_input.value, `   Autor: ` + autor_input.value, `   Lançamento: ` + ano_input.value);
+
+    musicas.push(musica_input.value + '');
+
+    localStorage.setItem("valueText", msc_output);
+    localStorage.setItem("musica", musicas);
+
+    musica_input.value = '';
+    autor_input.value = '';
+    ano_input.value = '';
+
+    console.log(musicas);
+
+};
+
+
+
+let btn = document.querySelector("#inputButton");
+
+
+let btn2 = document.querySelector("#inputButton2");
+
+function adiciona2() {
+    var paragrafo = document.querySelector("#viewSessionSotorage");
+    paragrafo.innerHTML = '';
+    let item = document.createElement("li");
+    let teste = localStorage.getItem('valueText');
+
+    let texto = document.createTextNode(teste);
+
+    item.appendChild(texto);
+    paragrafo.appendChild(item);
+
+
+    tam_msc += localStorage.getItem("musica");
+
+    console.log(tam_msc);
+
+    mostrardados();
+};
--- teste.js
+++ teste.js
@@ -6,27 +6,28 @@ const port = 3000;
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(
+    import.meta.url);
 const __dirname = dirname(__filename);
 
 
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: false
+    extended: false
 }));
 
 
 
 
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 
-app.post("/votoss", function(req, res){
+app.post("/votoss", function(req, res) {
 
-   res.send("Nome: "+req.body.votacao + " Tipo: "+req.body.comida);
+    res.send("Nome: " + req.body.votacao + " Tipo: " + req.body.comida);
 
 });
 
@@ -46,20 +47,26 @@ app.post("/votoss", function(req, res){
 
 
 
-app.get("/tarefa/:materia/:dificuldade", function(req, res){
-    res.send("A sua materia eh "+ req.params.materia +"<br>E a dificuldade dela eh "+req.params.dificuldade);
+
+
+
+
+
+
+app.get("/tarefa/:materia/:dificuldade", function(req, res) {
+    res.send("A sua materia eh " + req.params.materia + "<br>E a dificuldade dela eh " + req.params.dificuldade);
 });
 
-app.get("/inicio", function(req, res){
+app.get("/inicio", function(req, res) {
     res.send("Esta eh a página inicial...");
 });
 
-app.get("/bolo", function(req, res){
+app.get("/bolo", function(req, res) {
     res.send("Deu certo +_+");
 });
 
 
 
-app.listen(port, function(){
+app.listen(port, function() {
     console.log("Servidor rodando em http://localhost:" + port);
 });
