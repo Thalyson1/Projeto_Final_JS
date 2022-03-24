90fd12bec489270894c77e7a02c5d29802c4fac4
Thalyson1 <Trungr@hotmail.com>
2022-03-24 11:21:58 -0300

    Alterei o teste.js para funcionar o servidor na minha branch

--- teste.js
+++ teste.js
@@ -29,64 +29,54 @@ app.post("/enviar-musica", function(req, res) {
     let novaMusica = req.body;
     lista.musicas.push(novaMusica);
 
-    app.get("/pegar-lista", (req, res) => {
-        res.send(lista);
-        console.log(lista);
-    });
+    fs.writeFileSync("lista.json", JSON.stringify(lista, null, 2), "utf-8");
+    console.log(lista);
 
-    // app.post("/votoss", function(req, res){
+    res.redirect("/");
+});
 
-    //    res.send("Nome: "+req.body.votacao + " Tipo: "+req.body.comida);
-    //app.post("/votoss", function(req, res) {
+app.get("/pegar-lista", (req, res) => {
+    res.send(lista);
+    console.log(lista);
+});
 
-    //res.send("Nome: " + req.body.votacao + " Tipo: " + req.body.comida);
-    //fs.writeFileSync("lista.json", JSON.stringify(lista, null, 2), "utf-8");
-    //console.log(lista);
+// app.post("/votoss", function(req, res){
 
-    // res.redirect("/");
-    //});
+//    res.send("Nome: "+req.body.votacao + " Tipo: "+req.body.comida);
 
-    // });
+// });
 
-    // const ranking = [
-    //     {nome: "davi", pontos: 10 },
-    //     {nome: "ricardo", pontos: 0 },
-    //     {nome: "mathias", pontos: 50 },
-    //     {nome: "Lara", pontos: 100 },
-    // ]
+// const ranking = [
+//     {nome: "davi", pontos: 10 },
+//     {nome: "ricardo", pontos: 0 },
+//     {nome: "mathias", pontos: 50 },
+//     {nome: "Lara", pontos: 100 },
+// ]
 
-    // app.get("/ranking", function (req, res){
-    //     res.send(ranking);
-    // });
+// app.get("/ranking", function (req, res){
+//     res.send(ranking);
+// });
 
-    app.get("/listando.html", (req, res) => {
-        res.sendFile(__dirname + "/listando.html");
-    });
+app.get("/listando.html", (req, res) => {
+    res.sendFile(__dirname + "/listando.html");
+});
 
 
 
 
 
 
+app.get("/tarefa/:materia/:dificuldade", function(req, res) {
+    res.send("A sua materia eh " + req.params.materia + "<br>E a dificuldade dela eh " + req.params.dificuldade);
+});
+
+app.get("/inicio", function(req, res) {
+    res.send("Esta eh a página inicial...");
+});
 
 
 
 
-
-
-    app.get("/tarefa/:materia/:dificuldade", function(req, res) {
-        res.send("A sua materia eh " + req.params.materia + "<br>E a dificuldade dela eh " + req.params.dificuldade);
-    });
-
-    app.get("/inicio", function(req, res) {
-        res.send("Esta eh a página inicial...");
-    });
-
-    app.get("/bolo", function(req, res) {
-        res.send("Deu certo +_+");
-    });
-
-    app.listen(port, function() {
-        console.log("Servidor rodando em http://localhost:" + port);
-    })
+app.listen(port, function() {
+    console.log("Servidor rodando em http://localhost:" + port);
 });
